d78d5b736af8ce87ea98baee808d7d609e27de9d
acidburn429 <Rotebete22>
2020-06-12 16:25:21 +0200

    -schule ergänzt - andere orte - bugfix

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -6,17 +6,17 @@ import { ChatBotAction } from './state/action';
 import { ThemeProvider } from 'styled-components';
 
 export function ProgrammingChatBot() {
-  const dispatch = useDispatch<Dispatch<ChatBotAction>>();
-  const [height, setHeight] = useState(window.innerHeight);
-  const [width, setWidth] = useState(window.innerWidth);
-  React.useEffect(() => {
-    function updateHeight() {
-      setHeight(window.innerHeight);
-      setWidth(window.innerWidth);
-    }
-    window.addEventListener('resize', updateHeight);
-    return () => window.removeEventListener('resize', updateHeight);
-  });
+    const dispatch = useDispatch<Dispatch<ChatBotAction>>();
+    const [height, setHeight] = useState(window.innerHeight);
+    const [width, setWidth] = useState(window.innerWidth);
+    React.useEffect(() => {
+        function updateHeight() {
+            setHeight(window.innerHeight);
+            setWidth(window.innerWidth);
+        }
+        window.addEventListener('resize', updateHeight);
+        return () => window.removeEventListener('resize', updateHeight);
+    });
 
     const steps = [
         {
@@ -30,17 +30,17 @@ export function ProgrammingChatBot() {
             trigger: 'enter-name',
         },
         {
-          id: 'enter-name',
-          user: true,
-          trigger: 'ask-age',
+            id: 'enter-name',
+            user: true,
+            trigger: 'ask-age',
         },
         {
-          id: 'ask-age',
-          message: (args: MessageArgs) => {
-            dispatch({ type: 'setName', value: args.previousValue });
-              return `Hallo ${args.previousValue} 😀. Wie alt bist du?`;
-          },
-          trigger: 'enter-age',
+            id: 'ask-age',
+            message: (args: MessageArgs) => {
+                dispatch({ type: 'setName', value: args.previousValue });
+                return `Hallo ${args.previousValue} 😀. Wie alt bist du?`;
+            },
+            trigger: 'enter-age',
         },
         {
             id: 'enter-age',
@@ -54,58 +54,43 @@ export function ProgrammingChatBot() {
         },
         {
             id: 'select-happyness',
-          options: [
-            { value: 1, label: 'Gut 😀', trigger: 'happyness-good' },
-            { value: 2, label: 'Geht so 😐', trigger: 'happyness-bof' },
-              { value: 3, label: 'Schlecht 😟', trigger: 'happyness-bad' },
-              { value: 4, label: 'Wütend 😡', trigger: 'happyness-angry' },
-            { value: 5, label: 'Traurig 😢', trigger: 'happyness-sad' },
-          ],
+            options: [
+                { value: 1, label: 'Gut 😀', trigger: 'happyness-good' },
+                { value: 2, label: ' Geht so 😐', trigger: 'happyness-bof' },
+                { value: 3, label: 'Schlecht 😟', trigger: 'happyness-bad' },
+                { value: 4, label: 'Wütend 😡', trigger: 'happyness-angry' },
+                { value: 5, label: 'Traurig 😢', trigger: 'happyness-sad' },
+            ],
         },
         {
             id: 'happyness-good',
-            message: (args: MessageArgs) => {
-                dispatch({ type: 'setHappiness', value: parseInt(args.previousValue) });
-                return 'Das ist schön, ich freue mich für dich! Wenn du mich brauchst, ich bin hier 😀';
-            }
+            message: 'Das ist schön, ich freue mich für dich! Wenn du mich brauchst, ich bin hier 😀',
         },
         {
             id: 'happyness-sad',
-            message: (args: MessageArgs) => {
-                dispatch({ type: 'setHappiness', value: parseInt(args.previousValue) });
-                return 'Oh, du bist traurig, das tut mir Leid. Ist etwas passiert?';
-            },
+            message: 'Oh, du bist traurig, das tut mir Leid. Ist etwas passiert?',
             trigger: 'select-happend',
         },
         {
             id: 'happyness-angry',
-            message: (args: MessageArgs) => {
-                dispatch({ type: 'setHappiness', value: parseInt(args.previousValue) });
-                return 'Oh, du bist wütend 😡! Das tut mir Leid. Ist etwas passiert?';
-            },
+            message: 'Oh, du bist wütend 😡! Das tut mir Leid. Ist etwas passiert?',
             trigger: 'select-happend'
         },
         {
             id: 'happyness-bof',
-            message: (args: MessageArgs) => {
-                dispatch({ type: 'setHappiness', value: parseInt(args.previousValue) });
-                return 'Okay..., ist was passiert';
-            },
+            message: 'Okay..., ist was passiert?',
             trigger: 'select-happend',
         },
         {
             id: 'happyness-bad',
-            message: (args: MessageArgs) => {
-                dispatch({ type: 'setHappiness', value: parseInt(args.previousValue) });
-                return 'Oh, dir geht es schlecht 😟 ! Das tut mir Leid. Ist etwas passiert?';
-            },
+            message: 'Oh, dir geht es schlecht 😟 ! Das tut mir Leid. Ist etwas passiert?',
             trigger: 'select-happend',
         },
         {
             id: 'select-happend',
             options: [
-                {value: 1, label: 'Ja', trigger: 'happend-yes'},
-                {value: 2, label: 'Nein', trigger: 'happend-no'},
+                { value: 1, label: 'Ja', trigger: 'happend-yes' },
+                { value: 2, label: 'Nein', trigger: 'happend-no' },
             ]
         },
         {
@@ -354,13 +339,20 @@ export function ProgrammingChatBot() {
             options: [
                 { value: 1, label: 'Lehrer', trigger: 'school-problem-teacher' },
                 { value: 2, label: 'Andere Schüler', trigger: 'school-problem-students' },
-                { value: 3, label: 'Andere Person', trigger: 'school-problem-other-person' },
             ],
         },
         {
             id: 'school-problem-teacher',
             message: 'Mit Lehrern also 😟. Was ist los?',
-            trigger: 'school-problem-teacher-ask-quantity',
+            trigger: 'school-problem-teacher-ask-type',
+        },
+        {
+            id: 'school-problem-teacher-ask-type',
+            options: [
+                { value: 1, label: 'Schreit mich an', trigger: 'school-problem-teacher-ask-quantity' },
+                { value: 2, label: 'Ist ungerecht', trigger: 'school-problem-teacher-ask-quantity' },
+                { value: 3, label: 'Lacht mich aus', trigger: 'school-problem-teacher-ask-quantity' },
+            ],
         },
         {
             id: 'school-problem-teacher-ask-quantity',
@@ -394,90 +386,225 @@ export function ProgrammingChatBot() {
         {
             id: 'school-problem-teacher-asked-parents-no',
             message: 'Möchtest du mit jemanden darüber sprechen? ich kann dir eine Telefonnummer geben 😀',
+            trigger: 'school-problem-teacher-asked-parents-no-1',
+        },
+        {
+            id: 'school-problem-teacher-asked-parents-no-1',
+            message: 'Toll 😀! Ruf doch die Nummer gegen Kummer 116 111 an.',
+            trigger: 'school-problem-teacher-asked-parents-no-2',
+        },
+        {
+            id: 'school-problem-teacher-asked-parents-no-2',
+            message: 'Die können dir auf jeden Fall helfen.',
         },
         {
             id: 'school-problem-students',
-            message: 'Dummy msg',
+            message: 'Mit anderen Kindern, also. Was ist dann los?',
+            trigger: 'school-problem-students'
         },
         {
-            id: 'school-problem-other-person',
-            message: 'Dummy msg',
+            id: 'school-problem-students',
+            message: 'Mit anderen Kindern, also. Was ist denn los?',
+            trigger: 'select-school-problem-other-students'
+        },
+        {
+            id: 'select-school-problem-other-students',
+            options: [
+                { value: 1, label: 'Sind gemein zu mir', trigger: 'school-problem-other-students-quantity' },
+                { value: 2, label: 'lachen mich aus' , trigger: '' },
+                { value: 3, label: 'keine Freunde',  trigger: ''},
+                { value: 4, label: 'schlagen mich', trigger: ''},
+            ],
+        },
+        {
+            id: 'school-problem-other-students-quantity',
+            message: 'Passiert das oft?',
+            trigger: 'select-school-problem-other-students-quantity',
+        },
+        {
+            id: 'select-school-problem-other-students-quantity',
+            options: [
+                { value: 1, label: 'Ja ständig', trigger: 'school-problem-other-students-help-asked'},
+                { value: 1, label: 'Nein, nur ab und zu', trigger: 'school-problem-other-students-help-asked'},
+                { value: 1, label: 'Nein', trigger: 'school-problem-other-students-help-asked'},
+            ],
+        },
+        {
+            id: 'school-problem-other-students-help-asked',
+            message: 'Hast du das jemanden erzählt?',
+            trigger: 'select-school-problem-other-students-help-asked',
+        },
+        {
+            id: 'select-school-problem-other-students-help-asked',
+            options: [
+                { value: 1, label: 'Ja', trigger: 'school-problem-other-students-help-asked-yes' },
+                { value: 2, label: 'Nein', trigger: 'school-problem-other-students-help-asked-no' },
+            ],
+        },
+        {
+            id: 'school-problem-other-students-help-asked-yes',
+            message: 'Hat das Gespräch geholfen?',
+            trigger: 'select-school-problem-other-students-help-asked-helpful',
+        },
+        {
+            id: 'select-school-problem-other-students-help-asked-helpful',
+            options: [
+                { value: 1, label: 'Ja', trigger: 'school-problem-other-students-asked-helpful-yes' },
+                { value: 1, label: 'Nein', trigger: 'school-problem-other-students-asked-helpful-no' },
+            ],
+        },
+        {
+            id: 'school-problem-other-students-asked-helpful-yes',
+            message: 'Toll, das freut mich 😀. Wenn du mich nochmal brauchst, ich bin immer für dich da.',
+        },
+        {
+            id: 'school-problem-other-students-asked-helpful-no',
+            message: '🥺 Das ist doof. Möchtest du, dass ich dir helfe?',
+            trigger: 'select-school-problem-other-students-asked-helpful-want-number'
+        },
+        {
+            id: 'select-school-problem-other-students-asked-helpful-want-number',
+            options: [
+                { value: 1, label: 'Ja', trigger: 'select-school-problem-other-students-help-asked-yes' },
+                { value: 1, label: 'Nein', trigger: 'school-problem-other-students-asked-helpful-want-number-no' },
+            ],
+        },
+        {
+            id: 'school-problem-other-students-asked-helpful-want-number-no',
+            message: 'Okay, ich verstehe. Du kannst jederzeit wieder mit mir sprechen, ich bin immer hier.',
+        },
+        {
+            id: 'school-problem-other-students-help-asked-no',
+            message: 'Möchtest du mit jemanden darüber sprechen?',
+            trigger: 'select-school-problem-other-students-help-asked-want-talk',
+        },
+        {
+            id: 'select-school-problem-other-students-help-asked-want-talk',
+            options: [
+                { value: 1, label: 'Ja', trigger: 'select-school-problem-other-students-help-asked-yes'},
+                { value: 1, label: 'Nein', trigger: 'school-problem-other-students-asked-helpful-want-number-no'}
+            ],
+        },
+        {
+            id: 'select-school-problem-other-students-help-asked-yes',
+            message: 'Toll 😀! Ruf doch die Nummer gegen Kummer 116 111 an.',
+            trigger: 'select-school-problem-other-students-help-asked-yes-1',
+        },
+        {
+            id: 'select-school-problem-other-students-help-asked-yes-1',
+            message: 'Sie ist von Montag - Samstag von 14 - 20 Uhr + am Montag, Mittwoch und Donnerstag um 10 - 12 Uhr zu erreichen.',
+            trigger: 'select-school-problem-other-students-help-asked-yes-2',
+        },
+        {
+            id: 'select-school-problem-other-students-help-asked-yes-2',
+            message: 'Ich wünsch dir viel Erfolg. Wenn du mich noch einmal brauchst ich bin immer da für dich. 😀',
         },
         {
             id: 'where-else',
-            message: 'Was anderes,'
+            message: 'Möchtest du mit Leuten sprechen, die dir helfen können?',
+            trigger: 'select-where-else-talk',
         },
-  ];
+        {
+            id: 'select-where-else-talk',
+            options: [
+                { value: 1, label: 'Ja', trigger: 'where-else-talk-yes' },
+                { value: 1, label: 'Nein', trigger: 'where-else-talk-no' },
+            ],
+        },
+        {
+            id: 'where-else-talk-yes',
+            message: 'Find ich toll 😀!',
+            trigger: 'where-else-talk-yes-1',
+        },
+        {
+            id: 'where-else-talk-yes-1',
+            message: 'Hier ist die Nummer gegen Kummer: 116 111.',
+            trigger: 'where-else-talk-yes-2',
+        },
+        {
+            id: 'where-else-talk-yes-2',
+            message: 'Sie ist von Montag - Samstag von 14 - 20 Uhr + am Montag, Mittwoch und Donnerstag um 10 - 12 Uhr zu erreichen.',
+            trigger: 'where-else-talk-yes-3'
+        },
+        {
+            id: 'where-else-talk-yes-3',
+            message: 'War schön mit dir zu reden, ich wünsch dir alles Gute.',
+        },
+        {
+            id: 'where-else-talk-no',
+            message: 'Okay. Du kannst aber jedes mal wiederkommen, wenn du Probleme hast. Ich bin immer für dich da. 😀',
+        },
+    ];
 
-  const theme = {
-    background: '#f5f8fb',
-    fontFamily: 'Verdana',
-    headerBgColor: '#1F8ACF',
-    headerFontColor: '#fff',
-    headerFontSize: '15px',
-    botBubbleColor: '#89cdba',
-    botFontColor: '#fff',
-    userBubbleColor: '#89cdba',
-    userFontColor: '#fff',
-  };
-
-  const header = (
-    <div
-      style={{
-        backgroundColor: '#e4b665',
-        padding: 14,
-        color: 'white',
-        fontWeight: 'bold',
-        fontSize: 16,
-      }}
-    >
-      Botty
-    </div>
-  );
-
-  const chatBotProps = {
-    headerTitle: 'Botty',
-    botAvatar: require('./icon.png'),
-    steps: steps,
-    headerComponent: header,
-      bubbleOptionStyle: { backgroundColor: '#f8e7ca', color: 'black' }
+    const theme = {
+        background: '#f5f8fb',
+        fontFamily: 'Verdana',
+        headerBgColor: '#1F8ACF',
+        headerFontColor: '#fff',
+        headerFontSize: '15px',
+        botBubbleColor: '#89cdba',
+        botFontColor: '#fff',
+        userBubbleColor: '#89cdba',
+        userFontColor: '#fff',
     };
 
-  if (height <= 750 && width <= 600) {
-    return (
-      <ThemeProvider theme={theme}>
-        <ChatBot width="100%" floating={true} opened={true} {...chatBotProps} />
-      </ThemeProvider>
-    );
-  }
-
-  return (
-    <div
-      style={{
-        backgroundColor: '#e1f3ee',
-        width: '100%',
-        minHeight: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignSelf: 'center',
-        }}
-      >
-        <div style={{ flex: 1 }} />
-        <div>
-          <ThemeProvider theme={theme}>
-            <ChatBot {...chatBotProps} />
-          </ThemeProvider>
-        </div>
-        <div style={{ flex: 1 }} />
-      </div>
+    const header = (
+        <div
+            style={{
+                backgroundColor: '#e4b665',
+                padding: 14,
+                color: 'white',
+                fontWeight: 'bold',
+                fontSize: 16,
+            }}
+        >
+            Botty
     </div>
-  );
+    );
+
+    const chatBotProps = {
+        headerTitle: 'Botty',
+        botAvatar: require('./icon.png'),
+        steps: steps,
+        headerComponent: header,
+        bubbleOptionStyle: { backgroundColor: '#f8e7ca', color: 'black' }
+    };
+
+    if (height <= 750 && width <= 600) {
+        return (
+            <ThemeProvider theme={theme}>
+                <ChatBot width="100%" floating={true} opened={true} {...chatBotProps} />
+            </ThemeProvider>
+        );
+    }
+
+    return (
+        <div
+            style={{
+                backgroundColor: '#e1f3ee',
+                width: '100%',
+                minHeight: '100vh',
+                display: 'flex',
+                flexDirection: 'column',
+            }}
+        >
+            <div
+                style={{
+                    display: 'flex',
+                    flex: 1,
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignSelf: 'center',
+                }}
+            >
+                <div style={{ flex: 1 }} />
+                <div>
+                    <ThemeProvider theme={theme}>
+                        <ChatBot {...chatBotProps} />
+                    </ThemeProvider>
+                </div>
+                <div style={{ flex: 1 }} />
+            </div>
+        </div>
+    );
 }
