9e415c03c42404af1321a20b2f759cfce6458404
acidburn429 <Rotebete22>
2020-06-12 12:29:23 +0200

    Updates

---
+++ precache-manifest.192b6d68c2387bde2d1f2533ca4abd45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e3ebd31be6907b6426e1707ae8bcdff",
+    "url": "/AcidBurn429/Botty/index.html"
+  },
+  {
+    "revision": "9622c988ef45b0e9fb49",
+    "url": "/AcidBurn429/Botty/static/css/main.b574126d.chunk.css"
+  },
+  {
+    "revision": "d055e6d412344afd9e26",
+    "url": "/AcidBurn429/Botty/static/js/2.ba2eee97.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AcidBurn429/Botty/static/js/2.ba2eee97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9622c988ef45b0e9fb49",
+    "url": "/AcidBurn429/Botty/static/js/main.88dfb875.chunk.js"
+  },
+  {
+    "revision": "0d0d2162df477393db27",
+    "url": "/AcidBurn429/Botty/static/js/runtime-main.dd745223.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5a66f61676b47d2b94ad44e6dc9bddb7.js"
+  "/AcidBurn429/Botty/precache-manifest.192b6d68c2387bde2d1f2533ca4abd45.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AcidBurn429/Botty/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
