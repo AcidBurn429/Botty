3aa85e505742af1fd4f6ac56a8bd648dfd8acc29
André Kovac <me@andrekovac.com>
2020-06-12 11:51:50 +0200

    Delete unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
 import './App.css';
 import { ProgrammingChatBot } from './Chat';
-import { getUrlVars } from './urlArgs';
 import { createStore } from 'redux';
 import { chatBotReducer } from './state';
 import { Provider } from 'react-redux';
 
 function App() {
-  // display custom site if site argument passed to url
-  const args = getUrlVars();
-  if (args['site']) {
-    return (
-      <div
-        dangerouslySetInnerHTML={{
-          __html: args['site'],
-        }}
-      />
-    );
-  }
-
   const store = createStore(chatBotReducer);
 
   return (
--- src/state/index.ts
+++ src/state/index.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import { profileReducer } from './profile';
-import { codeReducer } from './code';
 
 export const chatBotReducer = combineReducers({
   profile: profileReducer,
-  code: codeReducer,
 });
 
 export type ChatBotState = NonNullable<Parameters<typeof chatBotReducer>[0]>;
